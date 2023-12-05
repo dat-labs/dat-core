849b34ebeb02f73c9878d451e6e6c1840696a919
Ankit Juneja <ankit.juneja@decision-tree.com>
2023-12-05 13:19:46 +0530

    Added ConnectorBase

--- connectors/sources/source_base.py
+++ connectors/sources/source_base.py
@@ -1,5 +1,5 @@
 import os
-from abc import ABC, abstractmethod
+from abc import abstractmethod
 from typing import (Any, Dict, Iterable, Iterator, List,
     Mapping, MutableMapping, Optional, Tuple, Union)
 import yaml
@@ -7,57 +7,15 @@ from utils import schema_validate
 from pydantic_models.dat_message import DatMessage
 from pydantic_models.dat_catalog import DatCatalog
 from pydantic_models.connector_specification import ConnectorSpecification
+from connectors.base import ConnectorBase
 
-class SourceBase(ABC):
+class SourceBase(ConnectorBase):
     """
     Base abstract Class for all sources
     """
-    _spec_file = None
 
     def __init__(self) -> None:
         super().__init__()
-    
-    @abstractmethod
-    def check_connection(self, config: Mapping[str, Any]) -> Tuple[bool, Optional[Any]]:
-        """
-        Based on the given config, it will check if connection to source can be
-        established.
-
-        Args:
-            config (Mapping[str, Any]): The user-provided configuration as specified by
-              the source's spec.
-
-        Returns:
-            Tuple[bool, Optional[Any]]: If the bool is True, then connection is established and
-            no errors are returned. Otherwise, the next item in the Tuple will contain error
-        """
-        pass
-
-    def spec(self) -> ConnectorSpecification:
-        """
-        Will return source specification
-        """
-        with open(self._spec_file, 'r') as f:
-            spec_json = yaml.safe_load(f)
-        print(spec_json)
-        return ConnectorSpecification.model_validate(spec_json)
-    
-    def check(self, config: Mapping[str, Any]) -> Dict:
-        """
-        This will verify that the passed configuration follows a given schema and
-          that connection can be established.
-
-        Args:
-            config (Mapping[str, Any]): The user-provided configuration as specified by 
-            the source's spec.
-
-        Returns:
-            Dict: TODO: Should be a DatConnectionStatus object
-        """
-        check_succeeded, error = self.check_connection(config)
-        if not check_succeeded:
-            # TODO: Raise proper error
-            raise
 
     def discover(self, config: Mapping[str, Any]) -> DatCatalog:
         """
