88a9b356258cfe2d05a5b3c2cd8795698c332c98
Ankit Juneja <87367788+ankit-dc@users.noreply.github.com>
2024-04-10 18:14:51 +0530

    Now processor is emitting proper logs (#9)
    emittig proper logs from data processor

--- dat_core/connectors/destinations/vector_db_helpers/utils.py
+++ dat_core/connectors/destinations/vector_db_helpers/utils.py
@@ -1,9 +1,27 @@
-
 import itertools
 from typing import Any, Iterable, Iterator, Tuple
 
 def create_chunks(iterable: Iterable[Any], batch_size: int) -> Iterator[Tuple[Any, ...]]:
-    """A helper function to break an iterable into chunks of size batch_size."""
+    """
+    A helper function to break an iterable into chunks of size batch_size.
+
+    Args:
+        iterable (Iterable[Any]): The iterable to be divided into chunks.
+        batch_size (int): The size of each chunk.
+
+    Returns:
+        Iterator[Tuple[Any, ...]]: An iterator that yields tuples of elements
+        from the iterable, where each tuple represents a chunk.
+
+    Example:
+        >>> numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+        >>> for chunk in create_chunks(numbers, 3):
+        ...     print(chunk)
+        (1, 2, 3)
+        (4, 5, 6)
+        (7, 8, 9)
+        (10,)
+    """
     it = iter(iterable)
     chunk = tuple(itertools.islice(it, batch_size))
     while chunk:
