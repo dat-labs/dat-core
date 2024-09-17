f56c68d7a1550da176f87220913ddc1feacea65e
Rahul Kumar <rahul.kumar@decision-tree.com>
2024-09-17 18:05:15 +0530

    A unique identifier for run will be present in stream metadata

--- dat_core/pydantic_models/stream_metadata.py
+++ dat_core/pydantic_models/stream_metadata.py
@@ -18,3 +18,4 @@ class StreamMetadata(BaseModel):
     dat_document_entity: Optional[str] = None
     dat_last_modified: Optional[int] = None
     dat_document_chunk: Optional[str] = None
+    dat_run_id: str
